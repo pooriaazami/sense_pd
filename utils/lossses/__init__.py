7eb5e8ae21649ada5513e15487807fd973ece0fd
Pooria <pooriaazami@gmail.com>
2026-07-15 15:41:42 +0330

    loss functions copied form DPPD

---
+++ utils/lossses/__init__.py
@@ -0,0 +1,14 @@
+from .loss import loss_mpjpe, n_mpjpe, loss_velocity_feat, \
+    loss_velocity, loss_limb_var, loss_limb_gt, loss_angle, \
+    loss_angle_velocity
+
+__all__ = [
+    'loss_mpjpe', 
+    'n_mpjpe', 
+    'loss_velocity_feat', 
+    'loss_velocity', 
+    'loss_limb_var', 
+    'loss_limb_gt', 
+    'loss_angle', 
+    'loss_angle_velocity'  
+]
