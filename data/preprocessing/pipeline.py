2c003786f1d92af73171792dc1d6d0a05ab0972f
Pooria <pooriaazami@gmail.com>
2026-07-27 15:33:33 +0330

    file reorder + gitignore update

---
+++ data/preprocessing/pipeline.py
@@ -0,0 +1,37 @@
+import torch
+
+import numpy as np
+
+class Normalizer:
+    @staticmethod
+    def compute_bones(paths):
+        bones = []
+        for path in paths:
+            l = len(path)
+            for i, j in zip(range(l - 1), range(1, l)):
+                bones.append([path[i], path[j]])
+
+        return bones
+    
+    def __init__(self, paths):
+        self.bones_idx = Normalizer.compute_bones(paths)
+
+    def normalize_pose(self, pose: torch.Tensor):
+        """
+        pose: Input sequence with shape (T, J, 3)
+        """
+
+        pose = pose - pose[:, 0, :].unsqueeze(1)
+        
+        bones = pose[:, self.bones_idx, :]
+        bone_lengths = bones[:, :, 0, :] - bones[:, :, 1, :]
+        s = np.linalg.norm(bone_lengths, axis=1).mean()
+        pose = pose / s
+
+        pose = pose - pose[0, 0, 2]
+        mins = torch.amin(pose, dim=(0, 1))
+        maxs = torch.amax(pose, dim=(0, 1))
+
+        pose = 2 * (pose - mins) / (maxs - mins + 1e-8) - 1
+        
+        return pose
