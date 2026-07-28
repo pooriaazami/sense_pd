53d0145dfec0aa416922a4da1a91a2ff129306ea
Pooria <pooriaazami@gmail.com>
2026-07-29 01:53:46 +0330

    fixing an error

--- finetune_classifier.py
+++ finetune_classifier.py
@@ -61,7 +61,7 @@ def main():
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     backbone = MotionAGFormer(**convert_params(config.model)).to(device)
-    classifier = nn.Linear(config.model.dim_rep * config.data.num_joints, NUM_CLASSES)
+    classifier = nn.Linear(config.model.dim_rep * config.data.num_joints, NUM_CLASSES).to(device)
 
     optimizer = optim.AdamW(chain(backbone.parameters(), classifier.parameters()), lr=config.training.lr)
 
