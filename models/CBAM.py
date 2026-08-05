cc0653de4925a910a6427ec15dea2c8de0e7b6f8
Pooria <pooriaazami@gmail.com>
2026-08-05 20:00:44 +0330

    changing the CBAM module

--- models/CBAM.py
+++ models/CBAM.py
@@ -60,10 +60,8 @@ class CBAMClassificationHead(nn.Module):
                  num_classes):
         super().__init__()
 
-        self.cbam = nn.Sequential(
-            CBAM(channels=seq_length, r=r),
-            CBAM(channels=seq_length, r=r)
-        )
+        self.cbam = CBAM(channels=seq_length, r=r)
+        
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=conv_channels, kernel_size=1)
         self.head = nn.Linear(num_joints * seq_length * conv_channels, num_classes)
 
