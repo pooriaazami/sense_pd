57c2afc9fc3b0481b39ced32eb514458244648d1
Pooria <pooriaazami@gmail.com>
2026-08-05 19:21:33 +0330

    stacking CBAM layers

--- models/CBAM.py
+++ models/CBAM.py
@@ -60,7 +60,10 @@ class CBAMClassificationHead(nn.Module):
                  num_classes):
         super().__init__()
 
-        self.cbam = CBAM(channels=seq_length, r=r)
+        self.cbam = nn.Sequential(
+            CBAM(channels=seq_length, r=r),
+            CBAM(channels=seq_length, r=r)
+        )
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=conv_channels, kernel_size=1)
         self.head = nn.Linear(num_joints * seq_length * conv_channels, num_classes)
 
